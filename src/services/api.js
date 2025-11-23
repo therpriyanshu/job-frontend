41c2a4b6464b2302a9a34a9a00892978d0c50e65
therpriyanshu <priyanshukr102004@gmail.com>
2025-11-23 23:51:32 +0530

    Added .env

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log("Backend URL:", process.env.REACT_APP_BACKEND);
+
 const API = axios.create({
   baseURL: process.env.REACT_APP_BACKEND,
   withCredentials: true,
