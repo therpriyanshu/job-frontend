04808a907e9b4b03be40d07275bd2bd0e0d846a3
therpriyanshu <priyanshukr102004@gmail.com>
2025-11-24 00:19:09 +0530

    route updated

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 console.log("Backend URL:", import.meta.env.VITE_BACKEND_URL);
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: import.meta.env.VITE_BACKEND_URL + "/api",
   withCredentials: true,
 });
 
